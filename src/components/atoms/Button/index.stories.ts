86bdc3290259d6edd2d25f7819c925d22fcab1d8
kecy <keishiro.oyama@kecy.me>
2023-07-30 21:29:36 +0900

    feat: add Button component

---
+++ src/components/atoms/Button/index.stories.ts
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './index';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'Atoms/Button',
+  component: Button,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    children: { control: 'text' },
+    buttonType: {
+      control: 'select',
+      options: [
+        'primary',
+        'danger',
+        'neutral-filled',
+        'neutral-outlined',
+        'neutral-text',
+      ],
+    },
+    size: {
+      control: 'select',
+      options: [
+        'sm',
+        'md',
+        'lg',
+      ],
+    },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    buttonType: 'primary',
+    size: 'md',
+    children: 'アクション',
+  },
+};
---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,113 @@
+import { ButtonHTMLAttributes, FC } from 'react';
+import clsx from 'clsx';
+
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: string;
+  buttonType?:
+    | 'primary'
+    | 'danger'
+    | 'neutral-filled'
+    | 'neutral-outlined'
+    | 'neutral-text';
+  size?: 'sm' | 'md' | 'lg';
+  loading?: boolean;
+  className?: string;
+};
+
+export const Button: FC<Props> = ({
+  children,
+  buttonType = 'neutral-filled',
+  type = 'button',
+  color = 'primary',
+  size = 'md',
+  loading = false,
+  className,
+  ...otherProps
+}) => {
+  const shapeClassName = {
+    'rounded-lg': buttonType !== 'neutral-text',
+    border: buttonType === 'neutral-outlined',
+  };
+
+  const colorClassName =
+    buttonType === 'primary'
+      ? 'bg-primary text-on-primary hover:opacity-80'
+      : buttonType === 'danger'
+      ? 'bg-error text-on-error hover:opacity-80'
+      : buttonType === 'neutral-outlined'
+      ? 'border-outline-variant bg-surface text-on-surface hover:opacity-70'
+      : buttonType === 'neutral-text'
+      ? 'text-on-surface hover:opacity-70'
+      : 'bg-on-surface text-surface hover:opacity-80';
+
+  const sizeClassName =
+    size === 'lg'
+      ? 'text-base leading-4'
+      : size === 'sm'
+      ? 'text-xs leading-3'
+      : 'text-sm leading-[0.875rem]';
+
+  const paddingClassName =
+    buttonType !== 'neutral-text'
+      ? size === 'lg'
+        ? 'px-5 py-3'
+        : size === 'sm'
+        ? 'px-3 py-1.5'
+        : 'px-4 py-2'
+      : size === 'lg'
+      ? 'px-2 py-2'
+      : size === 'sm'
+      ? 'px-1 py-1'
+      : 'px-2 py-2';
+
+  const fontClassName =
+    buttonType === 'neutral-text' ? 'font-medium' : 'font-bold';
+
+  const borderClassName = buttonType === 'neutral-outlined' ? 'border' : '';
+
+  const gapClassName = size === 'sm' ? 'gap-1' : size === 'lg' ? 'gap-2' : 'gap-2';
+
+  const loadingColorClassName =
+    buttonType === 'primary' ? 'border-on-primary'
+    : buttonType === 'danger' ? 'border-on-error'
+    : buttonType === 'neutral-outlined' ? 'border-on-surface'
+    : buttonType === 'neutral-text' ? 'border-on-surface'
+    : 'border-surface';
+
+  const loadingSizeName =
+    size === 'sm' ? 'h-3 w-3 border-2'
+    : size === 'lg' ? 'h-5 w-5 border-2'
+    : 'h-4 w-4 border-2';
+
+  return (
+    <button
+      type={type}
+      className={clsx(
+        'group relative flex items-center justify-center',
+        gapClassName,
+        fontClassName,
+        paddingClassName,
+        shapeClassName,
+        borderClassName,
+        colorClassName,
+        sizeClassName,
+        className
+      )}
+      data-loading={loading}
+      disabled={loading}
+      {...otherProps}
+    >
+      <span className="group-data-[loading=true]:invisible">{children}</span>
+      <span className="hidden group-data-[loading=true]:inline-block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <span
+          className={clsx(
+            'animate-spin rounded-full border-t-transparent',
+            sizeClassName,
+            loadingColorClassName,
+            loadingSizeName,
+          )}
+        />
+      </span>
+    </button>
+  );
+};
