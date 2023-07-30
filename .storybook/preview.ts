41e05165539f58505ccadcfd1daab15a14f6a34c
kecy <keishiro.oyama@kecy.me>
2023-07-30 17:08:38 +0900

    chore: add web fonts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,19 @@
 import type { Preview } from '@storybook/react';
 
-import '../src/globals.css';
+
+// Noto Sans JP
+import '@fontsource/noto-sans-jp/300.css';
+import '@fontsource/noto-sans-jp/400.css';
+import '@fontsource/noto-sans-jp/500.css';
+import '@fontsource/noto-sans-jp/700.css';
+
+// Roboto Mono
+import '@fontsource/roboto-mono/400.css';
+
+// Inter
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/700.css';
 
 const preview: Preview = {
   parameters: {
