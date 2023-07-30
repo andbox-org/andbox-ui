69e0f45a7c8961faa86ef218bcbabeb90579ff74
kecy <keishiro.oyama@kecy.me>
2023-07-30 14:15:38 +0900

    chore: add color theme seting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import { tailwindConfigColors } from './src/theme';
+
 module.exports = {
   prefix: 'and-',
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    colors: tailwindConfigColors,
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
