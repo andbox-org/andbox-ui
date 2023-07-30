673adfbe22a5876b7b5b802e4c973617e4188e32
kecy <keishiro.oyama@kecy.me>
2023-07-30 17:49:27 +0900

    feat: add Badge component

---
+++ src/components/atoms/Badge/index.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+
+type BadgeProps = {
+  text: string;
+  className?: string;
+};
+
+export const Badge: FC<BadgeProps> = ({ text, className }) => {
+  return (
+    <div className={`
+      inline-flex items-center rounded-full 
+      bg-surface-container
+      px-2 py-0.5 
+      text-xs font-medium text-on-surface font-sans
+      ring-1 ring-inset ring-outline-variant
+      ${className}
+    `}>
+      {text}
+    </div>
+  );
+};
