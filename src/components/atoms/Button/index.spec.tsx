adff24b8563b95915e422c36c80b3e81efcaa4fb
Keishiro Oyama <keishiro.oyama@kecy.me>
2023-08-05 13:07:55 +0900

    chore: add tests for Button components

---
+++ src/components/atoms/Button/index.spec.tsx
@@ -0,0 +1,32 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { Button } from './index';
+
+describe('Button', () => {
+  it('renders', () => {
+    const { container } = render(<Button>Test</Button>);
+    expect(container).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  it('should be disabled when `disabled` prop is true', () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick} disabled>Test</Button>);
+    fireEvent.click(screen.getByRole('button'));
+    expect(onClick).not.toHaveBeenCalled();
+  });
+
+  it('should be disabled when `loading` prop is true', () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick} loading>Test</Button>);
+    fireEvent.click(screen.getByRole('button'));
+    expect(onClick).not.toHaveBeenCalled();
+  });
+
+  it('calls onClick when clicked', () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>Test</Button>);
+    fireEvent.click(screen.getByRole('button'));
+    expect(onClick).toHaveBeenCalled();
+  });
+});
