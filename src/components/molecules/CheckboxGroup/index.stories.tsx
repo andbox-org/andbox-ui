494a8e2e96772f53a7b663a71e4d51e4f459cc52
Keishiro Oyama <keishiro.oyama@kecy.me>
2023-08-05 17:16:40 +0900

    chore: change props types (string->ReactNode) in CheckboxGroup

--- src/components/molecules/CheckboxGroup/index.stories.tsx
+++ src/components/molecules/CheckboxGroup/index.stories.tsx
@@ -1,7 +1,7 @@
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { CheckboxGroup } from './index';
-import { symbol } from 'zod';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -16,7 +16,9 @@ const meta = {
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     symbol: { control: 'select', options: [ 'check', 'dash' ] },
-    label: { control: 'text' },
+    children: { control: 'text' },
+    supportSlot: { control: 'text' },
+    errorSlot: { control: 'text' },
     disabled: { control: 'boolean' },
     error: { control: 'boolean' },
   },
@@ -28,14 +30,14 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Example: Story = {
   args: {
-    label: '利用規約に同意しますか？',
+    children: <><a href="#" className="text-primary underline">利用規約</a>に同意しますか？</>,
     symbol: 'check',
   },
 };
 
 export const Disabled: Story = {
   args: {
-    label: '利用規約に同意しますか？',
+    children: '利用規約に同意しますか？',
     symbol: 'check',
     disabled: true,
   },
@@ -43,19 +45,19 @@ export const Disabled: Story = {
 
 export const WithSupportText: Story = {
   args: {
-    label: '利用規約に同意しますか？',
+    children: '利用規約に同意しますか？',
     symbol: 'check',
-    supportText: '未成年の方は保護者の同意を得てからチェックしてください。',
+    supportSlot: '未成年の方は保護者の同意を得てからチェックしてください。',
   },
 };
 
 
 export const Error: Story = {
   args: {
-    label: '利用規約に同意しますか？',
+    children: '利用規約に同意しますか？',
     symbol: 'check',
-    supportText: '未成年の方は保護者の同意を得てからチェックしてください。',
+    supportSlot: '未成年の方は保護者の同意を得てからチェックしてください。',
     error: true,
-    errorText: 'サービスを利用するには利用規約に同意する必要があります。',
+    errorSlot: 'サービスを利用するには利用規約に同意する必要があります。',
   },
 };
--- src/components/molecules/CheckboxGroup/index.tsx
+++ src/components/molecules/CheckboxGroup/index.tsx
@@ -1,19 +1,19 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import clsx from 'clsx';
 
 import { Checkbox, CheckboxProps } from '../../atoms/Checkbox';
 
 export type CheckboxGroupProps = CheckboxProps & {
-  label: string;
-  supportText?: string;
-  errorText?: string;
+  children: ReactNode;
+  supportSlot?: ReactNode;
+  errorSlot?: ReactNode;
   className?: string;
 };
 
 export const CheckboxGroup: FC<CheckboxGroupProps> = ({
-  label,
-  supportText,
-  errorText,
+  children,
+  supportSlot,
+  errorSlot,
   error,
   disabled,
   className,
@@ -23,11 +23,11 @@ export const CheckboxGroup: FC<CheckboxGroupProps> = ({
     <div className={clsx('flex', 'flex-col', 'gap-2', 'group')} data-error={error} aria-disabled={disabled}>
       <label className={clsx(['flex', 'items-center', 'gap-2', 'cursor-pointer', 'group-aria-disabled:cursor-not-allowed'], className)}>
         <Checkbox {...props} error={error} disabled={disabled} />
-        { label && <span className={clsx('font-sans', 'text-sm', 'group-aria-disabled:opacity-70')}>{label}</span> }
+        <span className={clsx('font-sans', 'text-sm', 'group-aria-disabled:opacity-70')}>{children}</span>
       </label>
       <div className={clsx('ml-7', 'flex', 'flex-col', 'gap-2')}>
-        { supportText && <p className={clsx('font-sans', 'text-sm', 'text-on-surface')}>{supportText}</p> }
-        { errorText && <p className={clsx('font-sans', 'text-sm', 'text-error', 'hidden', 'group-data-[error=true]:block')}>{errorText}</p> }
+        { supportSlot && <p className={clsx('font-sans', 'text-sm', 'text-on-surface')}>{supportSlot}</p> }
+        { errorSlot && <p className={clsx('font-sans', 'text-sm', 'text-error', 'hidden', 'group-data-[error=true]:block')}>{errorSlot}</p> }
       </div>
     </div>
   );
