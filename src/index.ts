16994f8c8f7c60941941edb1023bce91b606bc3e
Keishiro Oyama <keishiro.oyama@kecy.me>
2023-08-20 19:34:31 +0900

    chore: export new components

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ export * from './components/atoms/Badge';
 export * from './components/atoms/Button';
 export * from './components/atoms/Card';
 export * from './components/atoms/Checkbox';
-export * from './components/atoms/IconButton';
 export * from './components/atoms/DropdownMenuTrigger';
+export * from './components/atoms/IconButton';
+export * from './components/atoms/Input';
+export * from './components/atoms/Section';
+export * from './components/atoms/Supplementary';
+
 export * from './components/molecules/CheckboxGroup';
+export * from './components/molecules/InputGroup';
+export * from './components/molecules/Main';
 export * from './components/molecules/UserSummary';
