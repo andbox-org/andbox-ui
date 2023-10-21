9977290635289393c0f9336ad1e8cd2094e6878c
kecy <keishiro.oyama@kecy.me>
2023-10-21 15:44:16 +0900

    chore: improve gap in Main

--- src/components/molecules/Main/_Root.tsx
+++ src/components/molecules/Main/_Root.tsx
@@ -8,7 +8,7 @@ export type MainRootProps = {
 
 export const Root: FC<MainRootProps> = ({ children, className }) => {
   return (
-    <main className={clsx('flex', 'flex-col', 'gap-16', className)}>
+    <main className={clsx('flex', 'flex-col', 'gap-10', className)}>
       {children}
     </main>
   );
